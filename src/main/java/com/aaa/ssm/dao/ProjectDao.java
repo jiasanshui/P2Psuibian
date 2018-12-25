b7ed755d8f5ecc30f75b9757aa9af431938a2572
jiasanshui <158690262@qq.com>
2018-12-25 21:44:43 +0800

    1111111

--- src/main/java/com/aaa/ssm/dao/ProjectDao.java
+++ src/main/java/com/aaa/ssm/dao/ProjectDao.java
@@ -33,8 +33,16 @@ public interface ProjectDao {
      *查询全部项目
      * @return
      */
-    @Select("select borrowid,applicant,tel,timelimit,purpose,des,quantity,cost,CONCAT(TO_CHAR(apr*100,'990.99'),'%') apr,borrowmoney,danbaostyle,payment,username,stateid,borrownum,winbidmoney,concat((round(winbidmoney/borrowmoney,4)*100),'%') jindu,round(winbidmoney/borrowmoney*10) jindua  from borrow")
-    List<Map> getHouseProAll();
+    @Select("<script>select * from (select rownum rn,borrowid,applicant,tel,timelimit,purpose,des,quantity,cost," +
+            "CONCAT(TO_CHAR(apr*100,'990.99'),'%') apr,borrowmoney,danbaostyle,payment,username," +
+            "stateid,borrownum,winbidmoney,concat((round(winbidmoney/borrowmoney,4)*100),'%') jindu," +
+            "round(winbidmoney/borrowmoney*10) jindua from borrow where rownum &lt; #{end} " +
+            "<if test=\"pt!=null and pt!=''\"> and danbaostyle=#{danbao}</if>" +
+            "<if test=\"bi!=null and bi!=''\"> and apr &gt;= #{startapr} and apr &lt;= #{endapr}</if>" +
+            "<if test=\"sm!=null and sm!=''\"> and timelimit &gt;= #{startlimit} and timelimit &lt;= #{endlimit}</if>" +
+            "<if test=\"rs!=null and rs!=''\"> and payment=#{repayment}</if>" +
+            ") a where a.rn &gt; #{start} </script>")
+    List<Map> getHouseProAll(Map map);
 
     /**
      * 根据编号查询招标进度
@@ -45,4 +53,17 @@ public interface ProjectDao {
             "CONCAT(TO_CHAR(apr*100,'990.99'),'%') apr,borrowmoney,danbaostyle,payment,username,stateid," +
             "borrownum,winbidmoney from borrow where rownum<3")
     List<Map> getJinduByNum(String borrowNum);
+
+    /**
+     * 获取分页总数量
+     * @param map
+     * @return
+     */
+    @Select("<script> select count(*) cnt from borrow where 1=1 " +
+            "<if test=\"pt!=null and pt!=''\"> and danbaostyle=#{danbao}</if>" +
+            "<if test=\"bi!=null and bi!=''\"> and apr &gt;= #{startapr} and apr &lt;= #{endapr}</if>" +
+            "<if test=\"sm!=null and sm!=''\"> and timelimit &gt;= #{startlimit} and timelimit &lt;= #{endlimit}</if>" +
+            "<if test=\"rs!=null and rs!=''\"> and payment=#{repayment}</if> " +
+            "</script>")
+    int getPageCount(Map map);
 }
