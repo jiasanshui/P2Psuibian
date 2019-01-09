0fdf2ea05547a2193c0371b3d89abd1a296a0735
qy88xfz <767299286@qq.com>
2019-01-09 21:00:52 +0800

    Merge branch 'master' of https://github.com/orgqy88groupthree/P2Psuibian
    # Conflicts:
    #	src/main/resources/templates/qiantai/account_setting.html

--- src/main/java/com/aaa/ssm/service/impl/AccountFlowServiceImpl.java
+++ src/main/java/com/aaa/ssm/service/impl/AccountFlowServiceImpl.java
@@ -34,4 +34,42 @@ public class AccountFlowServiceImpl implements AccountFlowService {
     public int update(Map map) {
         return accountFlowDao.update(map);
     }
+
+    /**
+     * 查询流水类型
+     * @return
+     */
+    @Override
+    public List<Map> getFlowtype() {
+        return accountFlowDao.getFlowtype();
+    }
+
+    /**
+     * 获取分页总数量
+     * @param map
+     * @return
+     */
+    @Override
+    public int getPageCount(Map map) {
+        List<Map> pageCount = accountFlowDao.getPageCount(map);
+        //判断集合，如果不为空，返回总数量
+        if(pageCount!=null&&pageCount.size()>0){
+            return Integer.valueOf(pageCount.get(0).get("CNT")+"");
+        }
+        return 0;
+    }
+
+    /**
+     * 获取资金记录流水分页
+     * @param map
+     * @return
+     */
+    @Override
+    public List<Map> getAccountFlow(Map map) {
+        int pageNo=map.get("pageNo")==null?1:Integer.valueOf(map.get("pageNo")+"");
+        int pageSize = map.get("pageSize")==null?10:Integer.valueOf(map.get("pageSize")+"");
+        map.put("start",(pageNo-1)*pageSize);
+        map.put("end",pageNo*pageSize+1);
+        return accountFlowDao.getAccountFlow(map);
+    }
 }
