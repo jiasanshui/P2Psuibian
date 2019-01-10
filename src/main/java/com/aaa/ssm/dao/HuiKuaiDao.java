1a2a98448e5c2d2504aec3e8e7f71ebc067d300a
qy88xfz <767299286@qq.com>
2019-01-11 00:17:25 +0800

    Merge branch 'master' of https://github.com/orgqy88groupthree/P2Psuibian
    # Conflicts:
    #	src/main/java/com/aaa/ssm/controller/AccountFlowController.java
    #	src/main/java/com/aaa/ssm/controller/JumpController.java

--- src/main/java/com/aaa/ssm/dao/HuiKuaiDao.java
+++ src/main/java/com/aaa/ssm/dao/HuiKuaiDao.java
@@ -9,7 +9,7 @@ import java.util.Map;
 public interface HuiKuaiDao {
     /**
      * 查询每个用户每笔回款信息
-     * @param userId
+     * @param map
      * @return
      */
     @Select("<script> select * from (select rownum rn,t.tamount,b.tapr,to_char(add_months(r.starttime,b.timelimit),'yyyy-mm-dd') " +
@@ -17,7 +17,10 @@ public interface HuiKuaiDao {
             "b.des,b.timelimit,b.danbaostyle from tender t " +
             "left join borrow b on t.borrownum=b.borrownum left join " +
             "repayinfo r on b.borrownum=r.borrownum where t.userid=#{userId} and rownum &lt; #{end} " +
-            "<if test=\"starttime!=null and starttime!=''\"> and r.starttime=#{startTime}</if>) " +
+            " <if test=\"selecttoday!=null and selecttoday!=''\">  and trunc(flowdate)=trunc(sysdate) </if> " +
+            " <if test=\"selectseven!=null and selectseven!=''\">  and flowdate > sysdate - interval '7' day </if> " +
+            " <if test=\"selectmonth!=null and selectmonth!=''\">  and flowdate > sysdate - interval '1' month </if> "  +
+            " <if test=\"selectsix!=null and selectsix!=''\">  and flowdate > sysdate - interval '6' month </if>) " +
             "a where a.rn &gt; #{start}</script>")
     List<Map> getHuiKuaiList(Map map);
     /**
@@ -26,6 +29,9 @@ public interface HuiKuaiDao {
      * @return
              */
     @Select("<script> select count(*) cnt from tender where 1=1 and userid=#{userId} " +
-            "<if test=\"endtime!=null and endtime!=''\"> and b.endtime=#{endTime}</if></script>")
+            " <if test=\"selecttoday!=null and selecttoday!=''\">  and trunc(flowdate)=trunc(sysdate) </if> " +
+            " <if test=\"selectseven!=null and selectseven!=''\">  and flowdate > sysdate - interval '7' day </if> " +
+            " <if test=\"selectmonth!=null and selectmonth!=''\">  and flowdate > sysdate - interval '1' month </if> " +
+            " <if test=\"selectsix!=null and selectsix!=''\">  and flowdate > sysdate - interval '6' month </if></script>")
     List<Map> getPageCount(Map map);
 }
