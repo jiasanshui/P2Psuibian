6b51dc7473f6c8a93d9e92febda8445de615e846
qy88xfz <767299286@qq.com>
2019-01-12 08:28:27 +0800

    Merge branch 'master' of https://github.com/orgqy88groupthree/P2Psuibian
    # Conflicts:
    #	src/main/java/com/aaa/ssm/controller/AccountFlowController.java
    #	src/main/java/com/aaa/ssm/controller/JumpController.java

--- src/main/java/com/aaa/ssm/service/HuiKuanService.java
+++ src/main/java/com/aaa/ssm/service/HuiKuanService.java
@@ -5,29 +5,9 @@ import java.util.Map;
 
 public interface HuiKuanService {
     /**
-     * 查询每个用户每笔回款信息
+     * 账户总揽中回款计划(按月付息，到期还本、等额本息)
      * @param map
      * @return
      */
-    List<Map> getHuiKuaiList(Map map);
-
-    /**
-     * 获取分页总数量
-     * @param map
-     * @return
-     */
-    int getPageCount(Map map);
-    /**
-     * 到期付本付息
-     * @param map
-     * @return
-     */
-    List<Map> getHuiKuanList(Map map);
-
-    /**
-     * 查询还款方式
-     * @param map
-     * @return
-     */
-    List<Map> getpaymentList(Map map);
+    List<Map> gethuaikuanList(Map map);
 }
